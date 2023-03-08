3fe7dcbea2dfe1641ff3cb37a29856972f996a11
chevyB <chevbanico@gmail.com>
2023-03-09 01:18:48 +0800

    Tailwind, mantine hooks, prettier, eslint. husky

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import Home from "./pages/Home";
+import Layout from "./pages/common/Layout";
+import AdminDashboard from "./pages/admin/Dashboard";
+import Missing from "./pages/common/Missing";
+import Unauthorized from "./pages/common/Unauthorized";
+import RequireAuth from "./pages/common/RequireAuth";
+import { Routes, Route } from "react-router-dom";
+
+const ROLES = {
+  User: "user",
+  Admin: "admin",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* we want to protect these routes */}
+        <Route element={<RequireAuth allowedRoles={[ROLES.User]} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]} />}>
+          <Route path="admin" element={<AdminDashboard />} />
+        </Route>
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/auth/Register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <article style={{ padding: "100px" }}>
+      <h1>Login!</h1>
+    </article>
+  );
+};
+
+export default Register;
