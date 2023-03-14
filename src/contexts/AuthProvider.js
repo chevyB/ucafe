1a3e025f13513e27fffd4868943dd7968e8664f6
chevyB <chevbanico@gmail.com>
2023-03-14 23:26:33 +0800

    Add to Cart

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react"
 import { auth, db } from "../api/base"
 import { onAuthStateChanged } from "firebase/auth"
-import { getDoc, doc } from "firebase/firestore"
+import { getDoc, doc, collection, where, query, getDocs } from "firebase/firestore"
 import { LoadingOverlay } from "@mantine/core"
 import { useLocation, useNavigate } from "react-router-dom"
 import { PUBLIC_URLS, ROLES } from "../configs/config"
@@ -14,8 +14,24 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState({})
   const [homeLink, setHomeLink] = useState("/")
   const [pending, setPending] = useState(true)
+  const [triggerCartFetch, setTriggerCartFetch] = useState(false)
+  const [userCart, setUserCart] = useState(0)
 
   console.log({ user })
+
+  const getUserCart = async () => {
+    const userCart = await getDocs(
+      query(collection(db, "carts"), where("user_id", "==", user.id))
+    )
+    setUserCart(userCart)
+  }
+
+  useEffect(() => {
+    if (user?.role === ROLES.User) {
+      getUserCart()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [triggerCartFetch])
 
   useEffect(() => {
     onAuthStateChanged(auth, (authUser) => {
@@ -60,6 +76,10 @@ export const AuthProvider = ({ children }) => {
           setHomeLink("/")
           break
       }
+
+      if (user.role === ROLES.User) {
+        getUserCart()
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user])
@@ -76,7 +96,9 @@ export const AuthProvider = ({ children }) => {
   }
 
   return (
-    <AuthContext.Provider value={{ user, setUser, homeLink }}>
+    <AuthContext.Provider
+      value={{ user, setUser, homeLink, setTriggerCartFetch, userCart }}
+    >
       {children}
     </AuthContext.Provider>
   )
--- src/pages/components/templates/Template.js
+++ src/pages/components/templates/Template.js
@@ -1,3 +1,5 @@
+import { Indicator } from "@mantine/core"
+import { IconShoppingBag } from "@tabler/icons-react"
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { auth } from "../../../api/base"
@@ -5,7 +7,7 @@ import { ROLES } from "../../../configs/config"
 import useAuth from "../../../hooks/useAuth"
 
 const Template = ({ children }) => {
-  const { setUser, user, homeLink } = useAuth()
+  const { setUser, user, homeLink, userCart } = useAuth()
   const navigate = useNavigate()
   const [showSideBar, setShowSideBar] = useState(false)
 
@@ -22,84 +24,100 @@ const Template = ({ children }) => {
           <Link to={homeLink}>
             <img className="h-6" src="/logo.png" alt="logo" />
           </Link>
-          <button
-            onClick={() => setShowSideBar(!showSideBar)}
-            data-collapse-toggle="navbar-solid-bg"
-            type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-solid-bg"
-            aria-expanded="false"
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-6 h-6"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+          <div className="flex items-center">
+            {user.role === ROLES.User && (
+              <Indicator
+                inline
+                color="red"
+                className="mx-3"
+                size={10}
+                label={userCart.size}
+                disabled={!userCart.size}
+              >
+                <IconShoppingBag />
+              </Indicator>
+            )}
+            <button
+              onClick={() => setShowSideBar(!showSideBar)}
+              data-collapse-toggle="navbar-solid-bg"
+              type="button"
+              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-solid-bg"
+              aria-expanded="false"
             >
-              <path
-                fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div
-            className={`${
-              showSideBar ? "absolute top-16 left-0 z-20 bg-gray-200 p-4" : "hidden"
-            }  w-full md:block md:w-auto `}
-          >
-            <ul className="flex flex-col rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-white md:bg-transparent">
-              {user.email && (
-                <li>
-                  <Link
-                    to={homeLink}
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <div
+              className={`${
+                showSideBar
+                  ? "absolute top-16 left-0 z-20 bg-gray-200 p-4"
+                  : "hidden"
+              }  w-full md:block md:w-auto `}
+            >
+              <ul className="flex flex-col rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-white md:bg-transparent">
+                {user.email && (
+                  <li>
+                    <Link
+                      to={homeLink}
+                      className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                )}
+                {user.role === ROLES.User && (
+                  <li>
+                    <Link
+                      to="/orders"
+                      className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
+                    >
+                      Orders
+                    </Link>
+                  </li>
+                )}
+                {user.role === ROLES.Seller && (
+                  <li>
+                    <Link
+                      to="/seller/orders"
+                      className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
+                    >
+                      Orders
+                    </Link>
+                  </li>
+                )}
+                {user.role === ROLES.Seller && (
+                  <li>
+                    <Link
+                      to="/seller/product/add"
+                      className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
+                    >
+                      Add Product
+                    </Link>
+                  </li>
+                )}
+                <li onClick={logout}>
+                  <div
+                    type="button"
                     className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
                   >
-                    Home
-                  </Link>
+                    Logout
+                  </div>
                 </li>
-              )}
-              {user.role === ROLES.User && (
-                <li>
-                  <Link
-                    to="/orders"
-                    className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
-                  >
-                    Orders
-                  </Link>
-                </li>
-              )}
-              {user.role === ROLES.Seller && (
-                <li>
-                  <Link
-                    to="/seller/orders"
-                    className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
-                  >
-                    Orders
-                  </Link>
-                </li>
-              )}
-              {user.role === ROLES.Seller && (
-                <li>
-                  <Link
-                    to="/seller/product/add"
-                    className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
-                  >
-                    Add Product
-                  </Link>
-                </li>
-              )}
-              <li onClick={logout}>
-                <div
-                  type="button"
-                  className="cursor-pointer block py-2 pl-3 pr-4 text-gray-700 md:p-0 hover:text-sky-500"
-                >
-                  Logout
-                </div>
-              </li>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
