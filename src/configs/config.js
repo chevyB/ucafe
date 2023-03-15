474aafca29e351bafc32116d464296118d2f7b0f
chevyB <chevbanico@gmail.com>
2023-03-16 00:27:07 +0800

    Create order

--- src/configs/config.js
+++ src/configs/config.js
@@ -4,4 +4,11 @@ export const ROLES = {
   Admin: "admin",
 }
 
+export const ORDER_STATUS = {
+  Receive: "recieve",
+  Preparing: "preparing",
+  Ready: "ready",
+  Completed: "completed",
+}
+
 export const PUBLIC_URLS = ["/login", "/register"]
